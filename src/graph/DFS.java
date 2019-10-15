493b91f9d3444452a3e70cb20ca71cb429a8985d
daniel.charbel <daniel.charbel@osolutions.tech>
2019-10-15 17:02:40 +0300

    Binary Search

---
+++ src/graph/DFS.java
@@ -0,0 +1,22 @@
+package graph;
+
+public class DFS {
+    public static void main(String[] args) {
+
+        Graph graph = new Graph();
+        boolean found = DFSRecursive(graph);
+
+    }
+
+    private static boolean DFSRecursive(Graph root){
+
+        return false;
+    }
+
+    public static class Graph {
+        int data;
+    }
+
+}
+
+
